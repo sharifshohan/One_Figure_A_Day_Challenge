50be4910b536ec2b1cd33d01eff71178644bbbde
Mohammad Umer Sharif Shohan <53044146+sharifshohan@users.noreply.github.com>
2021-05-05 16:39:08 +0600

    Update Day4_radar_chart.R

--- Day4_radar_chart.R
+++ Day4_radar_chart.R
@@ -1,19 +1,20 @@
 # Create data: note in High school for Jonathan:
 data <-as.data.frame(matrix( sample( 2:20 , 60 , replace=T) , ncol=10, byrow=TRUE))
-colnames(data) <- c("math" , "english" , "biology" , "computer science" , "R-coding", "data-viz" , "french" , "physic", "statistic", "sport" )
+colnames(data) <- c("Immunology" , "MolBio" , "Carbohydrate" , "Com science" , "Nucleic Acid", "Enzyme" , "Cancer" , "English", "Statistics", "Protein" )
 
 # To use the fmsb package, I have to add 2 lines to the dataframe: the max and min of each topic to show on the plot!
 data <-rbind(rep(20,10) , rep(0,10) , data)
-
+data
 # Prepare color
 colors_border=colormap(colormap=colormaps$viridis, nshades=6, alpha=1)
 colors_in=colormap(colormap=colormaps$viridis, nshades=6, alpha=0.3)
 
 # Prepare title
-mytitle <- c("Hasib", "Galib", "Sharif", "Taif", "Jakaria", "Rezwan")
+mytitle <- c("Hasib", "Galib", "Rezwan", "Taif", "Jakaria", "Mahmud")
 
 # Split the screen in 6 parts
 par(mar=rep(0.8,4))
+par
 par(mfrow=c(2,3))
 
 # Loop for each plot
@@ -35,3 +36,9 @@ for(i in 1:6){
                   title=mytitle[i]
       )
 }
+
+
+
+
+
+
