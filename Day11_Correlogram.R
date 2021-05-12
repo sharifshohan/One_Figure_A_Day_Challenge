8fdd8d8da7f5c27fc92fe2f62c40a1f961ba45e2
Mohammad Umer Sharif Shohan <53044146+sharifshohan@users.noreply.github.com>
2021-05-12 18:27:54 +0600

    Add files via upload
    Correlogram let’s you examine the corellation of multiple continuous variables present in the same dataframe.
    //
    The figure is generated using “R” with public source code. The data is very famous “mtcars” dataset. The figure shows the correlation between the variables with specific values of correlation.

---
+++ Day11_Correlogram.R
@@ -0,0 +1,17 @@
+library(ggplot2)
+library(ggcorrplot)
+
+# Correlation matrix
+data(mtcars)
+corr <- round(cor(mtcars), 1)
+
+# Plot
+ggcorrplot(corr, hc.order = TRUE, 
+           type = "lower", 
+           lab = TRUE, 
+           lab_size = 3, 
+           method="circle", 
+           colors = c("tomato2", "white", "springgreen3"), 
+           title="Correlogram of mtcars", 
+           ggtheme=theme_bw)
+ggsave("test.tiff", units="in", width=12, height=6, dpi=300, compression = 'lzw')
