524435490f091b8acd97d3bfefce4bc26c14e48c
Mohammad Umer Sharif Shohan <53044146+sharifshohan@users.noreply.github.com>
2021-06-03 11:40:16 +0600

    Add files via upload

---
+++ Day28_Tuftle_Boxplot.R
@@ -0,0 +1,14 @@
+library(ggthemes)
+library(ggplot2)
+theme_set(theme_tufte())  # from ggthemes
+
+# plot
+g <- ggplot(mpg, aes(manufacturer, cty))
+g + geom_tufteboxplot() + 
+      theme(axis.text.x = element_text(angle=65, vjust=0.6)) + 
+      labs(title="Tufte Styled Boxplot", 
+           subtitle="City Mileage grouped by Class of vehicle",
+           caption="Source: mpg",
+           x="Class of Vehicle",
+           y="City Mileage")
+ggsave("test.tiff", units="in", width=12, height=6, dpi=300, compression = 'lzw')
